85aac50fc2720b5adaff26b71681ea261528bb16
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-06-17 20:33:30 +0200

    iteration1

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: [true, "Username is required."],
+      unique: true,
+    },
+    hashedPassword: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
